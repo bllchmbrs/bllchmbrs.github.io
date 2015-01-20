622ab467588d63fa63ff6dd301a8333ff5ff7377
anabranch <wac.chambers@gmail.com>
2015-01-19 17:23:19 -0800

    Update

---
+++ make-blog-post.py
@@ -0,0 +1,20 @@
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("Title")
+args = parser.parse_args()
+title = args.Title
+start_string = """---
+layout: post
+title:  "%s"
+date:   %s
+categories: projects
+---
+"""
+
+date_string = datetime.now().strftime("%Y-%m-%d-")
+
+with open("_drafts/" + date_string + title.replace(" ", "-") + ".md", 'wb') as f:
+    f.write(start_string % (title.replace(" ", "-"), date_string))
+
