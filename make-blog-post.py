bf99edb0c0cac732bc4c9e54fd3629bb2e54328f
billc <wchambers@ischool.berkeley.edu>
2023-05-29 09:50:35 -0700

    update

--- make-blog-post.py
+++ make-blog-post.py
@@ -13,8 +13,9 @@ categories: projects
 ---
 """
 
-date_string = datetime.now().strftime("%Y-%m-%d")
+date_string = str(datetime.now().strftime("%Y-%m-%d"))
 
-with open("_drafts/" + date_string + "-" + title.replace(" ", "-") + ".md", 'wb') as f:
+
+with open("_drafts/" + date_string + "-" + str(title).replace(" ", "-") + ".md", 'wt') as f:
     f.write(start_string % (title.replace(" ", "-"), date_string))
 
